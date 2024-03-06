061965ab5eeb0561fd708d258e03ec0e193f10b1
Misganaw-Berihun <misganawbmb@gmail.com>
2024-03-06 22:53:31 +0300

    insert data

---
+++ postgres/insert_data.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+rpath = os.path.abspath('../')
+if rpath not in sys.path:
+    sys.path.insert(0, rpath)
+
+import pandas as pd
+from sqlalchemy import create_engine
+from postgres.postgresql_connection import create_database_engine
+from postgres.tables.agriculture_table import create_agriculture_table
+
+
+def map_dataframe_to_agriculture_table(engine, dataframe):
+    table_name = 'agriculture_table'
+    dataframe.to_sql(table_name, engine, index=False, if_exists='replace')
